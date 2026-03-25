910c3c933baf32c705bfdcfa3508608ad8adafb8
v-oss891 <vanshj023@gmail.com>
2026-03-25 21:03:42 +0530

    Update solution for vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -5,12 +5,11 @@ def vector_norm_3d(v):
     Compute the Euclidean norm of 3D vector(s).
     """
     # Your code here
-    v = np.array(v)
-
-    if v.ndim == 1:
+    v=np.array(v)
+    if v.ndim==1:
         return np.linalg.norm(v)
     else:
-        return np.linalg.norm(v, axis=1)
+        return np.linalg.norm(v,axis=1)
     
     
     
